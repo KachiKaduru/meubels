826c7d6a305cc5027bbff14e02ec93d1211a0ac4
Kachii <kachikaduru@gmail.com>
2024-10-28 09:55:44 +0100

    MADE THEM ENV VARIABLEs

--- supabase.js
+++ supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ltaicxyqvbocveazkrsf.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0YWljeHlxdmJvY3ZlYXprcnNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg5OTY0NzEsImV4cCI6MjA0NDU3MjQ3MX0.qHlMvZViteiaj6aeEbs1sbeP1Sx7QI_gjqA6Q0kvBfM";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
